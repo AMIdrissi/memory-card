224526375026f9374f79ea05435572a2bca4572c
AMIdrissi <amidrissi2002@gmail.com>
2023-10-26 02:13:35 +0100

    reconstructed and managed to fetch something this time HAHAHAHA

--- src/fetchData.jsx
+++ src/fetchData.jsx
@@ -2,53 +2,25 @@ import { useEffect, useState } from "react";
 
 function GetInfo(){
 
-    const [data , setData] = useState({});
-    const fetchDataMap = async(recource)=> {
-        const promise = await fetch(recource);
-        const data = await promise.json().catch(err => {console.log("could not fetch ")});
-        const objects = new Map();
-        (data.results).map((object)=>{
-            objects.set(object.name,object.url)
-        })
-        return objects;
-    }
-    const fetchData = async(recource)=> {
-        const promise = await fetch(recource);
-        const data = await promise.json().catch(err => {console.log("could not fetch ")});
+    const [obj , setObj] = useState({});
+    const [imgSrc , setImgSrc] = useState('');
+    const dataFetch = async(recource)=>{
+        const fetchOperation = await fetch(recource);
+        const data = await fetchOperation.json();
         return data;
     }
 
-    const fetchFormPics = async()=>{
-        let links = [];
-        const urls = (await fetchDataMap('https://pokeapi.co/api/v2/pokemon')).forEach((key,value)=> {
-            if (key.length!==0) {
-                (fetchData(key)
-                .then(data => fetchData(data.forms[0].url)
-                                .then(data1=>links.push(data1.sprites.front_default))));
-            }
-        });
-        return links;
-    }
+    useEffect(() => {
+        dataFetch('https://pokeapi.co/api/v2/pokemon-form/').then(data => {
+            setObj(data.results[0]);
+            dataFetch(data.results[0].url).then(datax=> {console.log(datax.sprites.front_default);setImgSrc(datax.sprites.front_default);});
+        })
+    },[])
 
-    const [linkos , setLinkos] = useState([]);
-    useEffect(()=> { 
-        fetchFormPics().then(data => setLinkos(data)); 
-    },[linkos.length]) 
-    console.log(linkos);
-    return ( <div>
-        {
-            linkos.length === 0 ? (
-                <p>this is a fetch whatever{linkos.length} , Loading...</p>
-            ) : (
-                <div>
-                    
-                        {linkos.map((link , index) => {
-                        <img key={index} src={link} alt="" />
-                    })} 
-                </div>
-            )
-        }
-    </div> );
+    return <div>
+        <h2>name : {obj.name}</h2>
+        <img src={imgSrc} alt="" style={{height:"200px"}} />
+    </div>
 }
 
 export default GetInfo;
